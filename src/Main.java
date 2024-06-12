2e97668327dc3b4360048ac559b4f0158a642b1f
mouhamed sarr <148370657+fuelz99@users.noreply.github.com>
2024-06-12 16:29:26 +0200

    update adef code fonctionel

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,18 @@ public class Main {
     public static void main(String[] args) {
         if (args.length != 1) {
             System.out.println("Usage: java -jar student-grades.jar <path_to_csv>");
-            return;  // Arrêter le programme si le nombre d'arguments est incorrect
+            return;
         }
 
-        String csvFilePath = args[0];  // Récupérer le chemin du fichier CSV depuis les arguments
-        List<Etudiant> etudiants = CSVReader.readCSV(csvFilePath);  // Lire les étudiants depuis le fichier CSV
+        String csvFilePath = args[0];
 
-        DatabaseNotation.createTable();  // Créer la table des étudiants dans la base de données
-        DatabaseNotation.AjoutEtudiant(etudiants);  // Insérer les étudiants dans la base de données
+        CSVReader csvReader = new CSVReader();
+        List<Etudiant> etudiants = CSVReader.readCSV(csvFilePath);
 
-        System.out.println("Students have been successfully imported into the database.");  // Message de confirmation
+        DatabaseNotation.createTable();
+
+        DatabaseNotation.AjoutEtudiant(etudiants);
+
+        System.out.println("Les étudiants ont été importés avec succès dans la base de données.");
     }
 }
